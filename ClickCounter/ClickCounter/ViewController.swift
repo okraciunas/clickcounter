0e2eff931e69c854bafff49af5612f6759d59ed0
Leonardo Oliva Kraciunas <leonardo.kraciunas@ftd.com.br>
2019-01-06 22:42:34 -0200

    Adicionando label no storyboard e conectando a ViewController.

--- ClickCounter/ClickCounter/ViewController.swift
+++ ClickCounter/ClickCounter/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @objc func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+    }
 }
-
